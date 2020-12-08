c00e5e086cbd9d830ded95fa403d749cab37022c
Tom <tomyeoman.github@fastmail.com>
2020-12-08 14:04:07 +0000

    Part 2 - day 8

--- internal/day8/day8.go
+++ internal/day8/day8.go
@@ -28,6 +28,7 @@ func RunProgram(input []string) int {
 			currLine += 1
 		case "jmp":
 			currLine += arg
+		}
 
 		_, isFound := instructionsRan[currLine]
 		isEndReached = isFound
@@ -38,3 +39,74 @@ func RunProgram(input []string) int {
 
 	return acc
 }
+
+func TestInputTerminates(input []string) (bool, int) {
+	currExecutionLine := 0
+	accTotal := 0
+	runCount := 0
+	isEndReached := false
+	hasGracefullyTerminated := false
+
+	for !isEndReached {
+
+		currInstruction := strings.Split(input[currExecutionLine], " ")
+		operation := currInstruction[0]
+		arg, _ := strconv.Atoi(currInstruction[1])
+
+		switch operation {
+		case "nop":
+			currExecutionLine += 1
+		case "acc":
+			accTotal += arg
+			currExecutionLine += 1
+		case "jmp":
+			currExecutionLine += arg
+		}
+
+		// Deadlock detection, if we've ran over our instruction count
+		if runCount > len(input) {
+			isEndReached = true
+		}
+		// If we're trying to access an instruction, out of program bounds we've finished
+		if currExecutionLine >= len(input) {
+			isEndReached = true
+			hasGracefullyTerminated = true
+		}
+
+		runCount++
+	}
+
+	return hasGracefullyTerminated, accTotal
+}
+
+func RunProgramTwo(input []string) int {
+
+	// Get array entries that contain NOOP
+	for i, inp := range input {
+
+		// If a line is nop / jmp, we'll switch it to the other and test whether program terminates
+		inputCopy := make([]string, len(input))
+		copy(inputCopy, input)
+
+		currInstruction := strings.Split(inp, " ")
+		operation := currInstruction[0]
+		arg := currInstruction[1]
+
+		if operation == "nop" {
+			inputCopy[i] = "jmp " + arg
+		}
+		if operation == "jmp" {
+			inputCopy[i] = "nop " + arg
+		}
+
+		hasTerminated, acc := TestInputTerminates(inputCopy)
+
+		if hasTerminated {
+			return acc
+		}
+	}
+
+	return 0
+}
+
+// If we haven't
--- internal/day8/day8_test.go
+++ internal/day8/day8_test.go
@@ -26,21 +26,46 @@ func TestGetResult(t *testing.T) {
 			}
 
 			res := RunProgram(data)
-			assert.Equal(t, 5, res, "Expected to find 151 bags")
+			assert.Equal(t, 5, res)
 
 		})
-		t.Run("shiny gold bags", func(t *testing.T) {
+		t.Run("should fetch correct result for input", func(t *testing.T) {
 
 			data := ReadFromFile("fixtures/day8_input.txt")
 
 			res := RunProgram(data)
-			assert.Equal(t, 1675, res, "Expected to find 151 bags")
+			assert.Equal(t, 1675, res)
 
 		})
 
 	})
 	t.Run("Part 2", func(t *testing.T) {
+		t.Run("Should pass example input", func(t *testing.T) {
 
+			data := []string{
+				"nop +0",
+				"acc +1",
+				"jmp +4",
+				"acc +3",
+				"jmp -3",
+				"acc -99",
+				"acc +1",
+				"jmp -4",
+				"acc +6",
+			}
+
+			res := RunProgramTwo(data)
+			assert.Equal(t, 8, res)
+
+		})
+		t.Run("should fetch correct result for input", func(t *testing.T) {
+
+			data := ReadFromFile("fixtures/day8_input.txt")
+
+			res := RunProgramTwo(data)
+			assert.Equal(t, 1532, res)
+
+		})
 	})
 
 }
